11b3a2f99931dba21f4b4c7405e6583fe86a5b1b
ofk <ofkjpn+github@gmail.com>
2023-06-23 00:01:58 +0900

    Add useIsomorphicLayoutEffect and useEffectEvent

---
+++ src/useEffectEvent.ts
@@ -0,0 +1,16 @@
+import { useCallback, useRef } from 'react';
+
+import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
+
+// see https://github.com/reactjs/rfcs/pull/220
+export function useEffectEvent<T extends ((...args: never[]) => unknown) | undefined>(
+  callback: T
+): T {
+  const ref = useRef<T | null>(null);
+  useIsomorphicLayoutEffect(() => {
+    ref.current = callback;
+  }, [callback]);
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const fn = useCallback((...args: never[]) => ref.current!(...args), []);
+  return (callback ? fn : callback) as T;
+}
